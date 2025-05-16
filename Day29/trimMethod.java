8443897044e57c018d502b9f041b70133be60eb0
Muskan Shroti <shrotimuskan@gmail.com>
2025-05-16 21:42:56 +0530

    Add files via upload

---
+++ Day29/trimMethod.java
@@ -0,0 +1,17 @@
+public class trimMethod {
+    
+
+
+    
+// Let's Start here
+let sentence="Full Stack Developer Veer";
+
+if(sentence.trim()===""){
+    console.log("The number of words in the sentence is : 0");
+}
+else{
+    let words=sentence.trim().split(/\s+/);
+    console.log("The number of words in the sentence is :"+words.length);
+}
+
+}
