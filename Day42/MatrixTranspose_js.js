faa54f7deeed95281989ccbacbc7c986e9411965
Muskan Shroti <shrotimuskan@gmail.com>
2025-05-28 22:40:45 +0530

    Add files via upload

---
+++ Day42/MatrixTranspose_js.js
@@ -0,0 +1,12 @@
+let m1 = [
+    [1,2,3], 
+    [4,5,6], 
+    [7,8,9]
+]
+
+for(let i=0; i<m1.length; i++){
+    for(let j=0; j<m1[0].length; j++){
+        console.log(m1[j][i])
+    }
+    console.log()
+}
