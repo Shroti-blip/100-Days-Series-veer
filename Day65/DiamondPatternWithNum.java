67ac91a912c2e6a5c69018a6ddaa78f68117b281
Muskan Shroti <shrotimuskan@gmail.com>
2025-06-21 20:16:49 +0530

    Add files via upload

---
+++ Day65/DiamondPatternWithNum.java
@@ -0,0 +1,32 @@
+// package Day65;
+
+public class DiamondPatternWithNum {
+    
+ public static void diamondPattern(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=(n-i); j++){
+               System.out.print(" ");
+            }
+            
+            for(int j=1; j<=(2*i-1); j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+          for(int i=n-1; i>=1; i--){
+            for(int j=1; j<=(n-i); j++){
+               System.out.print(" ");
+            }
+            
+            for(int j=1; j<=(2*i-1); j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        diamondPattern(5);
+    }
+}
